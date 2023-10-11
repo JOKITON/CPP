199c71cc4b689d327bbca3a695bd5428d12b643f
Hedgedog <hedgedog@localhost.localdomain>
2023-10-12 00:05:36 +0200

    Added toFloat() tests for CPP02, ex01

--- CPP_Module_02/ex01/main.cpp
+++ CPP_Module_02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaizpuru <jaizpuru@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 17:53:46 by jaizpuru          #+#    #+#             */
-/*   Updated: 2023/09/28 00:01:09 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2023/10/12 00:02:47 by jaizpuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,16 @@ std::cout << "\na is " << a << std::endl;
 std::cout << "b is " << b << std::endl;
 std::cout << "c is " << c << std::endl;
 std::cout << "d is " << d << std::endl;
+std::cout << std::endl;
+
 std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 std::cout << "d is " << d.toInt() << " as integer\n" << std::endl;
+
+std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+std::cout << "d is " << d.toFloat() << " as float\n" << std::endl;
 return 0;
 }
