d323c06281363dabff5a9ce7f7326ccb268f4602
Jokin Aizpuru <99480973+JOKITON@users.noreply.github.com>
2023-10-24 20:14:06 +0200

    AMateria.hpp, added Copy Constructor.

--- CPP_Module_04/ex03/AMateria.hpp
+++ CPP_Module_04/ex03/AMateria.hpp
@@ -24,6 +24,7 @@ class AMateria
 		AMateria( );
 		AMateria(std::string const & type);
                 AMateria& operator=( AMateria& p );
+                AMateria( AMateria& p );
 		virtual ~AMateria( );
 		virtual std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
