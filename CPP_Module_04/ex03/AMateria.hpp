d7f92bf5bc908143e0edb5afa6481c38eb74a53b
Jokin Aizpuru <99480973+JOKITON@users.noreply.github.com>
2023-10-24 20:10:27 +0200

    AMateria.hpp, added Copy Overload function.

--- CPP_Module_04/ex03/AMateria.hpp
+++ CPP_Module_04/ex03/AMateria.hpp
@@ -23,6 +23,7 @@ class AMateria
 	public:
 		AMateria( );
 		AMateria(std::string const & type);
+                AMateria& operator=( AMateria& p );
 		virtual ~AMateria( );
 		virtual std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
