27713b11ce942f76e069635a9d29673861c0c7b9
JOKITON <jokinaizpuru28@gmail.com>
2023-11-23 22:07:15 +0100

    Started working on CPP09, things are getting difficult! Nice to hear!

---
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaizpuru <jaizpuru@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 21:48:09 by jaizpuru          #+#    #+#             */
+/*   Updated: 2023/11/23 21:48:32 by jaizpuru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+
+
+
+#endif
---
+++ CPP_Module_09/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaizpuru <jaizpuru@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 21:47:25 by jaizpuru          #+#    #+#             */
+/*   Updated: 2023/11/23 22:06:06 by jaizpuru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int	main( int ac, char **ar ) {
+
+	if (ac > 1) {// program takes 1 file as argument
+		checkInput(ar[1]); // check if file complies with "date | value"
+		/*
+			A valid date will have this format: Year-Month-Day (2004-06-13).
+			A valid value must be either a float or a positive integer : 0-1000.
+
+			- You must use a container to validate the exercise.
+			- Handle possible errors with appropiate methods, exceptions, return values...
+		*/
+
+		createOutput(); // Display on output result of the value multiplied by exchange rate according to date inside database.
+		/*
+			- If the date does not exist, use the closest lower date there is in the database.
+		*/
+	}
+	else
+		std::cerr << "Error: could not open file." << std::endl;
+}
